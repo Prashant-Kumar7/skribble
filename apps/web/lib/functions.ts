eb1fa3d3de52fb79878f7207a23c93c48f553b9e
Prashant Kumar <tumsab7777@gmail.com>
2025-06-01 10:43:26 +0530

    deploy vercel

--- apps/web/lib/functions.ts
+++ apps/web/lib/functions.ts
@@ -1,14 +1,16 @@
 
 import { createClient } from "redis";
 // export const redisClient = createClient();
+import dotenv from "dotenv"
 
+dotenv.config()
 
 export const redisClient = createClient({
     username: 'default',
-    password: 'sHyvGNlJNQyPof6qaQSrBvpv0k6pxwv3',
+    password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-12075.c114.us-east-1-4.ec2.redns.redis-cloud.com',
-        port: 12075
+        host: 'redis-14424.c83.us-east-1-2.ec2.redns.redis-cloud.com',
+        port: 14424
     }
 });
 
